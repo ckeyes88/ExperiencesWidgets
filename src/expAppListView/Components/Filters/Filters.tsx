f166cdb535ad8ad8bb44bda788a20c8e2f91dcf8
Minh <xiu.chau@gmail.com>
2020-04-20 22:45:50 -0700

    WIP: filter by

--- src/expAppListView/Components/Filters/Filters.tsx
+++ src/expAppListView/Components/Filters/Filters.tsx
@@ -1,30 +1,58 @@
 import { h, Component } from "preact";
+import { EventLookup } from "../../Containers/EventsListWidget/EventsListWidget";
+import { Select, OptionDefinition } from "../Select/Select";
+
+function createFilterOptionsFromSet(lookup: EventLookup): OptionDefinition[] {
+  const ids = Object.keys(lookup);
+  const options: OptionDefinition[] = [{ label: "All", value: "All" }];
+
+  for (let i = 0; i < ids.length; i++) {
+    const id = ids[i];
+    options.push({
+      label: lookup[id].name,
+      value: id,
+    });
+  }
+
+  return options;
+}
+
+export type FilterBy = "All" | string;
 
 export type SortKey = "Date" | "Name";
 
-export interface IFiltersProps {
+export type FiltersProps = {
+  /** Lookup so we can find useful info w/ event ID */
+  eventLookup: EventLookup;
+  /** Whether to show all or a specific event */
+  filterBy: FilterBy;
   /** What basis to sort the product list by */
   sortBy: SortKey;
   /** Total # of unique products */
   totalProducts: number;
   /** Sort by change handler */
   onSortChange(event: MouseEvent): void;
-}
+  /** Filter by event name or show all */
+  onFilterChange(event: MouseEvent): void;
+};
 
 /**
  * Renders all possible configuration affordances for the widget in a row. These 
  * affordances allow users to sort by name/date, or specify a category of products
  * to filter by.
  */
-export class Filters extends Component<IFiltersProps> {
+export class Filters extends Component<FiltersProps> {
   /**
    * Render the filter bar.
    */
   public render({
+    eventLookup,
+    filterBy,
+    onFilterChange,
     // totalProducts,
     // sortBy,
     // onSortChange,
-  }: IFiltersProps) {
+  }: FiltersProps) {
     // const productWord = totalProducts === 1
     //   ? "product"
     //   : "products";
@@ -36,6 +64,14 @@ export class Filters extends Component<IFiltersProps> {
     return (
       <div className="Filters">
         <div className="Container Filters-Container">
+          <div className="Filters-Dropdown">
+            Filter by:
+            <Select 
+              options={createFilterOptionsFromSet(eventLookup)}
+              onSelectOption={onFilterChange}
+              value={filterBy}
+            />
+          </div>
           {/* <div className="Filters-SortBy">
             Sort by:
             <select 
