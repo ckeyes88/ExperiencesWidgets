17e7adb3279de74953edf2f834f1afc9e6a21fdf
Brian Pak <bbouhm@gmail.com>
2020-08-19 15:10:00 -0700

    increase button widths

--- src/widgets.ts
+++ src/widgets.ts
@@ -1,27 +1,29 @@
-
 /**
  * Loads a specified widget code into the website
- * 
+ *
  * @param keyAttribute The attribute id that represents that widget
  * @param scriptPath The url to the widget code
  */
 function loadExpAppWidget(keyAttribute: string, scriptPath: string): void {
   const mountPoints = document.querySelectorAll(keyAttribute);
-  
+
   if (mountPoints.length > 0) {
     let script = document.createElement("script");
     let styles = document.createElement("link");
     styles.rel = "stylesheet";
     styles.href = scriptPath.replace(".js", ".css");
 
-    script.src = scriptPath;    
+    script.src = scriptPath;
     document.head.appendChild(styles);
     document.body.appendChild(script);
   }
 }
 // baseUrl for Development
-//const baseUrl = "__BASE_URL__/__ENV_NAME__";
+const baseUrl = "__BASE_URL__/__ENV_NAME__";
 // baseURL for Production
-const baseUrl = "__BASE_URL__/public/__ENV_NAME__";
+// const baseUrl = "__BASE_URL__/public/__ENV_NAME__";
 loadExpAppWidget("[data-expapp-gallery]", `${baseUrl}/expAppListView.js`);
-loadExpAppWidget("[data-expapp-calendar-booking-form]", `${baseUrl}/calendarBookingForm.js`);
+loadExpAppWidget(
+  "[data-expapp-calendar-booking-form]",
+  `${baseUrl}/calendarBookingForm.js`
+);
