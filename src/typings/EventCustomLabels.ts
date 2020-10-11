bfc802aa0c5116e97b16320f11ddfa77719f944c
Maksim Hranin <bitterfoxyfox@protonmail.com>
2020-10-11 15:00:45 -0700

    Cleanup comments

--- src/typings/EventCustomLabels.ts
+++ src/typings/EventCustomLabels.ts
@@ -2,37 +2,37 @@
  * Event custom labels as they are defined in ExperiencesAppLibs repo
  */
 export type EventCustomLabels = {
-    selectDatesLabel: string; //N/A
-    selectDateLabel: string; //V
-    quantityLabel: string; //N/A
-    bookButtonLabel: string; //V
-    reserveButtonLabel: string; //V
-    totalLabel: string; //V
-    confirmVariantsLabel: string; //V
-    singularUnitLabel: string; //???
-    pluralUnitLabel: string; //???
-    firstNameLabel: string; //V
-    lastNameLabel: string; //V
-    phoneNumberLabel: string; //???
-    emailLabel: string; //V
-    optionalFieldLabel: string; //???
-    requiredWarningLabel: string; //???
-    showSlotsRemainingLabel: boolean; //V
-    slotsRemainingLabel: string; //V
+    selectDatesLabel: string;
+    selectDateLabel: string;
+    quantityLabel: string;
+    bookButtonLabel: string;
+    reserveButtonLabel: string;
+    totalLabel: string;
+    confirmVariantsLabel: string;
+    singularUnitLabel: string;
+    pluralUnitLabel: string;
+    firstNameLabel: string;
+    lastNameLabel: string;
+    phoneNumberLabel: string;
+    emailLabel: string;
+    optionalFieldLabel: string;
+    requiredWarningLabel: string;
+    showSlotsRemainingLabel: boolean;
+    slotsRemainingLabel: string;
     previousWeekAriaLabel: string;
     nextWeekAriaLabel: string;
-    noUpcomingTimeSlotsLabel: string; //V
-    minQuantityLabel: string; //V
-    maxQuantityLabel: string; //V
-    bookingModalHeaderLabel: string; //V
-    perAttendeeStepLabel: string; //V
-    previousLabel: string; //???
-    nextLabel: string; //V
-    confirmReservationButtonLabel: string; //V
-    addLabel: string; //V
-    savedSpotLabel: string; //V
-    sentConfirmationLabel: string; //V
-    emailReminderLabel: string; //V
-    finalConfirmationLabel: string; //V
-    confirmationAndPaymentFollowUpLabel: string; //???
+    noUpcomingTimeSlotsLabel: string;
+    minQuantityLabel: string;
+    maxQuantityLabel: string;
+    bookingModalHeaderLabel: string;
+    perAttendeeStepLabel: string;
+    previousLabel: string;
+    nextLabel: string;
+    confirmReservationButtonLabel: string;
+    addLabel: string;
+    savedSpotLabel: string;
+    sentConfirmationLabel: string;
+    emailReminderLabel: string;
+    finalConfirmationLabel: string;
+    confirmationAndPaymentFollowUpLabel: string;
   };
