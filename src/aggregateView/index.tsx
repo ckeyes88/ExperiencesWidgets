dacf4f7d193bef178aa77541ebb3e387aa545e9f
Maksim Hranin <bitterfoxyfox@protonmail.com>
2020-11-19 20:00:55 -0800

    More mobile css, cleanup

--- src/aggregateView/index.tsx
+++ src/aggregateView/index.tsx
@@ -5,27 +5,17 @@ import { CalendarContainer } from "./Components/CalendarMain";
 mountComponent({
   dataName: "expapp-aggregate-view",
   formatData(data) {
-      console.log("data from expapp-aggregate-view is ", data);
-    // if (!data.calendarBookingFormShopUrl) {
-    //   throw new Error("You must specify the [data-expapp-shop-url] attribute for this widget to load");
-    // }
+    if (!data.aggregateViewShopUrl) {
+      throw new Error("You must specify the [data-expapp-shop-url] attribute for this widget to load");
+    }
 
-    // if (!data.calendarBookingFormBaseUrl) {
-    //   throw new Error("You must specify the [data-expapp-shop-url] attribute for this widget to load");
-    // }
-
-    // if (!data.calendarBookingFormProductId) {
-    //   throw new Error("You must specify a product id for this widget to work.");
-    // }
-    
-    // if (!data.calendarBookingFormProductId) {
-    //   throw new Error("You must specify a product id for this widget to work");
-    // }
+    if (!data.aggregateViewBaseUrl) {
+      throw new Error("You must specify the [data-expapp-shop-url] attribute for this widget to load");
+    }
 
     return { 
         ...data, 
         baseUrl: data.aggregateViewBaseUrl, 
-        storefrontAccessToken: data.aggregateViewStorefrontAccessToken,
         shopUrl: data.aggregateViewShopUrl, 
         languageCode: data.aggregateViewLanguageCode || "en-US",
     };
