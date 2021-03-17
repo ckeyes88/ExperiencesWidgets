ff5a88fbdd1180f0c60e3e4ab953e81774053de6
shannon-coding <shannon.coding@gmail.com>
2021-03-17 15:53:44 -0700

    Add NumberCarousel component

---
+++ src/fullPageBookingForm/Components/Common/Input/NumberCarousel.stories.tsx
@@ -0,0 +1,25 @@
+/** @jsx h */
+import { h } from "preact";
+import { NumberCarousel, NumberCarouselProps } from "./NumberCarousel";
+
+export default {
+  title: "Full Page Booking Form/Common/Inputs/Number Carousel",
+  component: NumberCarousel,
+};
+
+const defaultArgs: NumberCarouselProps = {
+  variantName: "Senior",
+  variantQty: 0,
+  variantQtyMaximum: 5,
+  onDecreaseClick: () => {},
+  onIncreaseClick: () => {},
+  onChange: (value: string) => {},
+};
+
+const Template = (args: NumberCarouselProps) => <NumberCarousel {...args} />;
+
+export const Basic = Template.bind({});
+
+Basic.args = {
+  ...defaultArgs,
+};
---
+++ src/fullPageBookingForm/Components/Common/Input/NumberCarousel.tsx
@@ -0,0 +1,62 @@
+/** @jsx h */
+import { h, FunctionComponent } from "preact";
+import { JSXInternal } from "preact/src/jsx";
+import "./NumberCarousel.scss";
+
+export type NumberCarouselProps = {
+  variantName: string;
+  variantQty: number;
+  variantQtyMaximum: number;
+  onIncreaseClick: () => void;
+  onDecreaseClick: () => void;
+  onChange: (value: string) => void;
+};
+
+export const NumberCarousel: FunctionComponent<NumberCarouselProps> = ({
+  variantName,
+  variantQty,
+  variantQtyMaximum,
+  onIncreaseClick,
+  onDecreaseClick,
+  onChange,
+}) => {
+  const handleChange: JSXInternal.DOMAttributes<
+    HTMLInputElement
+  >["onChange"] = (event) => {
+    onChange(event.currentTarget.value);
+  };
+  const decreasingButtonClassNames = ["number-carousel__button"];
+  const increasingButtonClassNames = [...decreasingButtonClassNames];
+
+  if (variantQty <= 0) {
+    decreasingButtonClassNames.push("number-carousel__button--disabled");
+  }
+
+  if (variantQty >= variantQtyMaximum) {
+    increasingButtonClassNames.push("number-carousel__button--disabled");
+  }
+  return (
+    <div className="number-carousel">
+      <button
+        className={decreasingButtonClassNames.join(" ")}
+        onClick={onIncreaseClick}
+      >
+        -
+      </button>
+      <input
+        className="number-carousel__input"
+        type="number"
+        name={variantName}
+        min={0}
+        value={variantQty}
+        onChange={handleChange}
+      />
+      <button
+        className={increasingButtonClassNames.join(" ")}
+        onClick={onDecreaseClick}
+      >
+        +
+      </button>
+    </div>
+  );
+};
