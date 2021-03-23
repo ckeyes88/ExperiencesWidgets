348c9a92cc71f88c8c71a0404309ef2f3bf34ea2
siongesteban <siong.esteban@gmail.com>
2021-03-23 23:22:32 +0800

    Allow page title to be inline with product thumbnail

--- src/fullPageBookingForm/Components/PageTitle/PageTitle.tsx
+++ src/fullPageBookingForm/Components/PageTitle/PageTitle.tsx
@@ -6,16 +6,26 @@ import "./PageTitle.scss";
 export type PageTitleProps = {
   title: string;
   thumbnailSrc: string;
+  inlineWithThumbnail?: boolean;
 };
 
 export const PageTitle: FunctionComponent<PageTitleProps> = ({
   title,
   thumbnailSrc,
-}) => (
-  <div className="page-title">
-    <img className="page-title__thumbnail" src={thumbnailSrc} />
-    <h1 className="page-title__text">
-      <TextStyle variant="display1" text={title} />
-    </h1>
-  </div>
-);
+  inlineWithThumbnail,
+}) => {
+  const classNames = ["page-title"];
+
+  if (inlineWithThumbnail) {
+    classNames.push("page-title--inline");
+  }
+
+  return (
+    <div className={classNames.join(" ")}>
+      <img className="page-title__thumbnail" src={thumbnailSrc} />
+      <h1 className="page-title__text">
+        <TextStyle variant="display1" text={title} />
+      </h1>
+    </div>
+  );
+};
