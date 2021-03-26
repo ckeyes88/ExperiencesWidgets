d0eb4e2f3ff22044ef16481452cac0c4e22fb16b
shannon-coding <shannon.coding@gmail.com>
2021-03-25 17:03:23 -0700

    Example zustand component/story

--- src/fullPageBookingForm/Components/Common/Input/NumberCarousel.stories.tsx
+++ src/fullPageBookingForm/Components/Common/Input/NumberCarousel.stories.tsx
@@ -1,33 +1,19 @@
 /** @jsx h */
 import { h } from "preact";
-import { NumberCarousel, NumberCarouselProps } from "./NumberCarousel";
+import { NumberCarousel, useNumberCarouselStore } from "./NumberCarousel";
 
 export default {
   title: "Full Page Booking Form/Common/Inputs/Number Carousel",
   component: NumberCarousel,
 };
 
-const defaultArgs: NumberCarouselProps = {
-  name: "Senior",
-  currentQty: 0,
-  qtyMaximum: 5,
-  onDecreaseClick: () => {},
-  onIncreaseClick: () => {},
-  onChange: (value: string) => {},
-  isDisabled: false,
-};
+const { setName, setValue, setQtyMaximum } = useNumberCarouselStore();
+setName("Test");
+setQtyMaximum(5);
+setValue("2");
 
-const Template = (args: NumberCarouselProps) => <NumberCarousel {...args} />;
+const Template = () => <NumberCarousel />;
 
 export const Basic = Template.bind({});
 
-Basic.args = {
-  ...defaultArgs,
-};
-
 export const Disabled = Template.bind({});
-
-Disabled.args = {
-  ...defaultArgs,
-  isDisabled: true,
-};
