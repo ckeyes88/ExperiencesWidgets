db3cf76a5f8ca1d7a5fedf60575613da55c84137
shannon-coding <shannon.coding@gmail.com>
2021-05-28 10:30:47 -0700

    Init calendar skeleton component

---
+++ src/fullPageBookingForm/Components/Common/CalendarSkeleton/CalendarSkeleton.tsx
@@ -0,0 +1,10 @@
+/** @jsx h */
+import { h, FunctionComponent } from "preact";
+
+export const CalendarSkeleton: FunctionComponent = () => {
+  return (
+    <div>
+      <p>calendar skeleton</p>
+    </div>
+  );
+};
