8d37d6c3c62bf30acbd1ba8948ee5e27a6770593
shannon-coding <shannon.coding@gmail.com>
2021-06-08 15:33:43 -0700

    Add hook to track card groups

--- src/fullPageBookingForm/Components/TimeslotGroup/TimeslotGroup.tsx
+++ src/fullPageBookingForm/Components/TimeslotGroup/TimeslotGroup.tsx
@@ -4,24 +4,43 @@ import moment from "moment-timezone";
 import { TextStyle } from "../Common/TextStyle";
 import { TimeslotCard, TimeslotCardProps } from "../TimeslotCard";
 import "./TimeslotGroup.scss";
+import { useEffect, useRef, useState } from "preact/hooks";
 
 export type TimeslotGroup = {
   timeslots: TimeslotCardProps[];
   lang: string;
+  onScrollTimeslots: (isActive: boolean, startsAt: Date) => void;
 };
 
 export const TimeslotGroup: FunctionComponent<TimeslotGroup> = ({
   lang,
   timeslots,
+  onScrollTimeslots,
 }) => {
+  const ref = useRef<HTMLDivElement>();
+  const [currentY, setCurrentY] = useState(0);
   const { startsAt, timezone } = timeslots[0];
   const formattedDayText = moment(startsAt)
     .tz(timezone)
     .locale(lang)
     .format("dddd, MMMM D");
 
+  useEffect(() => {
+    const onScroll = () => {
+      const currentY = ref.current?.getBoundingClientRect().y;
+      setCurrentY(currentY);
+
+      if (currentY >= 85 && currentY < 95) {
+        onScrollTimeslots(true, startsAt);
+      }
+    };
+    window.addEventListener("scroll", onScroll, true);
+
+    return () => window.removeEventListener("scroll", onScroll, true);
+  }, [currentY, ref]);
+
   return (
-    <div className="timeslot-group">
+    <div className="timeslot-group" ref={ref}>
       <div className="timeslot-group__header">
         <TextStyle variant="display2" text={formattedDayText} />
       </div>
